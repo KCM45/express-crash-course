eb7572f3bda3621ee00b7629b4b75d14e9dd8838
Kama Corey McKenzie <kcm@Kamas-MBP.attlocal.net>
2023-05-21 12:32:40 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+````;
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
